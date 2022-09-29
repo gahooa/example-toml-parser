d6d115ac2a3e72c658b4a048bec5164235bbbf8c
Jason Garber <jgarber@appcove.com>
2022-09-29 17:32:38 -0400

    Working toml parsing example

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.10-alpine
 
 # Example line to install pip packages
-RUN pip install requests
+RUN pip install toml rich
 
 WORKDIR /app
 
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # vim:fileencoding=utf-8:ts=4:sw=4:sts=4:expandtab
 
-print(123)
+
+import toml
+import rich.pretty
+
+
+with open('data.toml', 'rt') as f:
+    data = f.read()
+
+rich.pretty.pprint(toml.loads(data))
+
+
 
